27beed34c54f79b4bf2375dcc0bd368deef7d26c
motherfaker <jialei.jiang@hotmail.com>
2024-11-30 17:35:41 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Hono } from "hono";
+import { streamText } from "hono/streaming";
+import { EventSourceParserStream } from "eventsource-parser/stream";
+import { Ai } from "@cloudflare/workers-types";
 export interface Env {
   // If you set another name in wrangler.toml as the value for 'binding',
   // replace "AI" with the variable name you defined.
@@ -13,13 +16,35 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.get("/test", async (c) => {
-  console.log(c);
-  const response = await c.env.AI.run("@cf/google/gemma-7b-it-lora", {
-    prompt: "What is the origin of the phrase Hello, World",
-  });
+//
+app.get("/chat", async (c) => {
+  // 获取路径请求参数
+  const { msg = "你好" } = c.req.query();
+  console.log(msg);
+  const messages: RoleScopedChatInput[] = [
+    {
+      role: "user",
+      content: msg,
+    },
+  ];
 
-  return c.json(response);
+  const eventSourceStream = (await c.env.AI.run("@cf/google/gemma-7b-it-lora", {
+    messages,
+    stream: true,
+  })) as ReadableStream;
+  // EventSource stream is handy for local event sources, but we want to just stream text
+  const tokenStream = eventSourceStream
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new EventSourceParserStream());
+
+  return streamText(c, async (stream) => {
+    for await (const msg of tokenStream) {
+      if (msg.data !== "[DONE]") {
+        const data = JSON.parse(msg.data);
+        stream.write(data.response);
+      }
+    }
+  });
 });
 
 export default app;
